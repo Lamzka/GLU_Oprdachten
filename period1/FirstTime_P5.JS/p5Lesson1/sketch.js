5d2a51034a2f7fb9b22ca4c085580b2966a80ce8
Raymond Besson <144126270+Lamzka@users.noreply.github.com>
2023-09-15 09:37:45 +0200

    Add files via upload

---
+++ period1/FirstTime_P5.JS/p5Lesson1/sketch.js
@@ -0,0 +1,42 @@
+
+// i have no idea what this does
+// only that it does something with the gray canvas
+function setup() {
+  createCanvas(500, 500);
+}
+
+
+// 2D render thing, i think???
+function draw() {
+  background(220);
+
+
+  //funny rectangles
+  fill('red');
+rect(0, 0, 150, 40);
+
+fill('blue');
+rect(100, 0, 50, 200);
+
+fill('white');
+rect(0 ,40, 60, 100);
+
+fill('yellow')
+rect(60 ,40, 40, 100 );
+
+//shake thingy????
+//rect(random(100,5), random(190,365), 10,80);
+
+//text
+//fill(50)
+//textSize(40);
+//text('Test', 200, 200);
+//}
+
+
+
+
+
+
+
+
